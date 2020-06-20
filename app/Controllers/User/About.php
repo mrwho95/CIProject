81d94ae1aaad9e5790c7367874c0a070f1712f59
mrwho95 <dnkh903@gmail.com>
2020-06-20 20:24:10 +0800

    build home page and about page

--- app/Controllers/User/About.php
+++ app/Controllers/User/About.php
@@ -1,12 +1,15 @@
-<?php namespace App\Controllers;
+<?php 
+namespace App\Controllers\User;
 
-class Home extends BaseController
+use App\Controllers\BaseController;
+
+class About extends BaseController
 {
 	public function index()
 	{
 		// return base_url();
 		// return view('welcome_message');
-		return view('user/home');
+		return view('user/about');
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/user/about.php
@@ -0,0 +1,185 @@
+<?= $this->extend('layout/template') ?>
+
+<?= $this->section('content') ?>
+
+<!--? Hero Start -->
+<div class="slider-area ">
+	<div class="slider-height2 d-flex align-items-center">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="hero-cap hero-cap2 text-center">
+						<h2>About Us</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Hero End -->
+<!--? About Area start -->
+<section class="about-area about2 section-padding30">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col-xl-5 col-lg-6">
+				<div class="about-caption2 mb-50">
+					<h3>HOW WE MAKE USER EXPERIENCES</h3>
+					<div class="send-cv">
+						<a href="#">hire@colorlib.com</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-5 col-lg-5">
+				<div class="about-caption mb-50">
+					<h3>You can’t use up creativity. The more you use, the
+					more you have in your signifant mind.</h3>
+					<p class="pera1"><span>SALVADOR DALI</span> Digital Artisit</p>
+					<div class="experience">
+						<div class="year">
+							<span>05</span>
+						</div>
+						<div class="year-details"><p>YEARS OF<br> DIGITAL EXPERIENCE</p></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-between align-items-center">
+			<div class="col-lg-6 col-md-9">
+				<div class="about2-img">
+					<img src="assets/img/gallery/about2.png" alt="">
+				</div>
+			</div>
+			<div class="col-lg-5 col-md-9">
+				<div class="about2-caption mb-70">
+					<h5>Our Mission</h5>
+					<p>Brook presents your services with flexible nienti and multipurpose layouts. You can select vorite layouts & elements for particular projects with unlimited mizationi possibilities.</p>
+				</div>
+				<div class="about2-caption">
+					<h5>Our Mission</h5>
+					<p>Brook presents your services with flexible nienti and multipurpose layouts. You can select vorite layouts & elements for particular projects with unlimited mizationi possibilities.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--? Brand Shape  -->
+	<div class="about-shape2 d-none d-xl-block">
+		<img src="assets/img/gallery/about_shape2.png" alt="">
+	</div>
+</section>
+<!-- About Area End -->
+<!--? Testimonial Start -->
+<div class="testimonial-area testimonial-padding">
+	<div class="container ">
+		<div class="row d-flex justify-content-center">
+			<div class="col-xl-11 col-lg-11 col-md-9">
+				<div class="h1-testimonial-active">
+					<!-- Single Testimonial -->
+					<div class="single-testimonial text-center">
+						<!-- Testimonial Content -->
+						<div class="testimonial-caption ">
+							<div class="testimonial-top-cap">
+								<img src="assets/img/gallery/testimonial.png" alt="">
+								<p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis por incididunt ut labore et dolore mas. </p>
+							</div>
+							<!-- founder -->
+							<div class="testimonial-founder d-flex align-items-center justify-content-center">
+								<div class="founder-img">
+									<img src="assets/img/gallery/Homepage_testi.png" alt="">
+								</div>
+								<div class="founder-text">
+									<span>Jessya Inn</span>
+									<p>Chif Photographer</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Single Testimonial -->
+					<div class="single-testimonial text-center">
+						<!-- Testimonial Content -->
+						<div class="testimonial-caption ">
+							<div class="testimonial-top-cap">
+								<img src="assets/img/gallery/testimonial.png" alt="">
+								<p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis por incididunt ut labore et dolore mas. </p>
+							</div>
+							<!-- founder -->
+							<div class="testimonial-founder d-flex align-items-center justify-content-center">
+								<div class="founder-img">
+									<img src="assets/img/gallery/Homepage_testi.png" alt="">
+								</div>
+								<div class="founder-text">
+									<span>Jessya Inn</span>
+									<p>Chif Photographer</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Testimonial End -->
+<!--? Brand Area Start -->
+<section class="brand-area pb-bottom section-padding30">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<!-- Section Tittle -->
+				<div class="section-tittle mb-30">
+					<h2>Take a look <br>at our achivment</h2>
+					<p>Excepteur sint occaecat cupidatat. irure dolor in.</p>
+					<a href="#" class="btn">Get Start</a>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-6">
+				<div class="single-brand text-center mb-30">
+					<img src="assets/img/gallery/brand1.png" alt="">
+					<p>Behance award</p>
+					<p>Prize 2019</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-6">
+				<div class="single-brand text-center mb-30">
+					<img src="assets/img/gallery/brand02.png" alt="">
+					<p>Behance award</p>
+					<p>Prize 2019</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-6">
+				<div class="single-brand text-center mb-30">
+					<img src="assets/img/gallery/brand2.png" alt="">
+					<p>Behance award</p>
+					<p>Prize 2019</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-6">
+				<div class="single-brand text-center mb-30">
+					<img src="assets/img/gallery/brand3.png" alt="">
+					<p>Behance award</p>
+					<p>Prize 2019</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-6">
+				<div class="single-brand text-center mb-30">
+					<img src="assets/img/gallery/brand4.png" alt="">
+					<p>Behance award</p>
+					<p>Prize 2019</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-6">
+				<div class="single-brand text-center mb-30">
+					<img src="assets/img/gallery/brand1.png" alt="">
+					<p>Behance award</p>
+					<p>Prize 2019</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--? Brand Shape  -->
+	<div class="brand-shape d-none d-lg-block">
+		<img src="assets/img/gallery/brand_shape.png" alt="">
+	</div>
+</section>
+<!-- Brand Area End -->  
+
+<?= $this->endSection() ?>
